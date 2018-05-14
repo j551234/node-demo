215279e921b4828b0f4b7b71cd5672237201568b
Unknown <james850704@gmail.com>
2018-05-14 21:06:57 +0800

    complete crawler

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log('hello');
 console.log('fuck me');
+const Xray=require('x-ray');
+const x = Xray();
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html','table >tr',[
+{
+    no:'td:nth-child(2)',
+    coursename:'td:nth-child(4)',
+    teacher:'td:nth-child(5)',
+}
+]).write('./dist/course.json');
