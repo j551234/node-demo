2527d06ed31c26d555e10184897f3f0a6b01e043
Unknown <james850704@gmail.com>
2018-05-15 19:54:57 +0800

    first http server

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-console.log('hello');
-console.log('fuck me');
-const Xray=require('x-ray');
-const x = Xray();
-x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html','table >tr',[
-{
-    no:'td:nth-child(2)',
-    coursename:'td:nth-child(4)',
-    teacher:'td:nth-child(5)',
-}
-]).write('./dist/course.json');
+const Express =require('express');
+const app=Express();
+app.get('/',function(req,res){
+    console.log('Hello');
+    res.send('hello world');
+});
+app.listen(3000,function(){
+    console.log('start listen https://localhost:3000')
+});
