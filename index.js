93a8844ef1e690d37346cdc00240cfa5c4f7d42d
Unknown <james850704@gmail.com>
2018-05-15 20:44:45 +0800

    add image.png

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const Express =require('express');
 const app=Express();
+const helmet=require('helmet');
+const serveStatic =require('serve-static');
+
+app.use(helmet());
+app.use(serveStatic('public'));
+
+app.get('/', (req, res, next) => {
+    console.log('middleware');
+    next(); 
+   }); 
+
+
 app.get('/',function(req,res){
     console.log('Hello');
     res.send('hello world');
 });
+app.get('/home',function(req,res){
+    res.send('hello home');
+});
+
+
 app.listen(3000,function(){
     console.log('start listen https://localhost:3000')
 });
